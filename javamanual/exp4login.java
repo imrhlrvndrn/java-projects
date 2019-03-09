5a5d41d39391776ba9fd802a56b3fd3b18101af3
Rahul Ravindran <rahul1116@users.noreply.github.com>
2019-03-09 17:24:09 +0530

    Addition: added the exp4 student activity > login programs

---
+++ javamanual/exp4login.java
@@ -0,0 +1,34 @@
+// This program creates a login program using JFrame
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class exp4login extends JFrame implements ActionListener{
+    JLabel l,log;
+    JButton b;
+    JTextField t;
+    public static void main(String ar[]) {
+        JFrame e = new JFrame();
+        e.setVisible(true);
+        e.setSize(700,700);
+        e.setTitle("My Login");
+        e.setLayout(new GridLayout(2,2));
+        l = new JLabel("Login ID");
+        log = new JLabel("");
+        t = new JTextField();
+        b = new JButton("Submit");
+        e.add(l);
+        e.add(t);
+        e.add(b);
+        e.add(log);
+        b.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        String id = t.getText();
+        if (id.equals("rahul")) {
+            log.setText("User authenticated");
+        }
+    }
+
+}
